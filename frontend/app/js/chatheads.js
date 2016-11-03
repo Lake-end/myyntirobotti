514c61086e14ced6b16242e6acb55f384b3fecf0
Kannisto Markus <a1303389@myy.haaga-helia.fi>
2016-11-03 14:29:48 +0200

    Message laatikko liikkuu hienosti riippuen kummalla puolella on :):)):):):)

--- frontend/app/js/chatheads.js
+++ frontend/app/js/chatheads.js
@@ -1,23 +1,34 @@
 /**
+
  * Created by Sampsa on 23.10.2016.
+
  */
+
 (function($) {
 
+    var bodyWidth = $(document.body).width(),
+    middle = bodyWidth / 2,
+    offset = -10,
+    box = document.getElementById("msg");
     /* Chathead JS */
+
     $(".chat-head").draggable({
         opacity: 0.95,
         scope: "chathead",
         scroll: true
+
     }).on("dragstop", function (e) {
-        var bodyWidth = $(document.body).width(),
-            middle = bodyWidth / 2,
-            $this = $(this),
-            offset = -10;
+           var $this = $(this);
+
         if (middle > e.pageX) {
+
+            box.style.left = "20%";
             $this.animate({
                 left: -offset
             });
         } else {
+
+            box.style.left ="-450%";
             $this.animate({
                 left: bodyWidth + offset - $this.width()
             });
@@ -25,14 +36,17 @@
     });
 
     $('.chat-head').click(function (e) {
-
-            var chatMessage = $(this).find('.message');
-            if (!chatMessage.is(':visible')) {
-                $('.message:visible').hide();
-
+        var chatMessage = $(this).find('.message');
+        if (!chatMessage.is(':visible')) {
+            $('.message:visible').hide();
+            if(e.pageX > middle){
+                box.style.left = "-450%";
             }
+            else {
+                box.style.left = "20%";
+            }
+        }
             chatMessage.toggle(100);
-
     });
     $('#msg').on("click", function() {
         return false;
