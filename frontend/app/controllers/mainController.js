7d641fb6b315df998089a0e35c0af90f28559c6b
Kannisto Markus <a1303389@myy.haaga-helia.fi>
2016-11-15 14:03:39 +0200

    Linkit kysymyksiin jos loytyy

--- frontend/app/controllers/mainController.js
+++ frontend/app/controllers/mainController.js
@@ -1,5 +1,7 @@
 /**
+
  * Created by a1400223 on 20.9.2016.
+
  */
 
 app.controller('MainController', ['$scope', 'ChatWindow', '$timeout', '$log', '$interval','$http', function($scope, ChatWindow, $timeout, $log, $interval, $http) {
@@ -11,33 +13,39 @@ app.controller('MainController', ['$scope', 'ChatWindow', '$timeout', '$log', '$
         $scope.id=0;
 
         //if lause ajetaa, jos on olemassa "sessionId" niminen sessio. sessio luodaan metodilla: sessionStorage.setItem("nimi", "arvo");
-
         if(sessionStorage.getItem("sessionId")!=null){
             $log.info("sessio löytyy");
-
             //Tarkistetaan löytyykö sessiota
-
             var id = JSON.parse(sessionStorage.getItem("sessionId")).id;
-
             $http.get('/get-session/' + id).
             success(function(data){
-
                 //Haetaan kysymys, johon käyttäjä jäi
-
                 $http.get('/question/' + data.current_question).
                 success(function(data){
+                $scope.alku = data.question.search("http");
+                // Jos linkistä löytyy 'http', laitetaan <a href='...'>...</a>
+                if($scope.alku > -1){
+                    if(data.question.search(".fi") == -1){
+                        $scope.loppu = data.question.search(".com") + 4;
+                    } else if(data.question.search(".com") == -1){
+                        $scope.loppu = data.question.search(".fi") + 3;
+                    }
+                    $scope.testi = data.question;
+                    $scope.question = $scope.testi.substring(0, $scope.alku) + "<a href='" + $scope.testi.substring($scope.alku, $scope.loppu) + "'>" + $scope.testi.substring($scope.alku, $scope.loppu) + "</a>" + $scope.testi.substring($scope.loppu);
+                    }
+                    // Jos ei löydy 'http', kysymys kirjoitetaan normaalisti
+                     else{
                     $scope.question = data.question;
+                    }
                     $scope.options = data.answers;
                     $scope.id=$scope.data.id;
                 }).error(function(err) {
                     $log.info("Tapahtui virhe");
                 })
                 $log.info(data);
-
             }).error(function(err) {
                 $log.info("Tapahtui virhe");
             })
-
         }
         else{
             //Alla oleva funktio luo uuden session ja tallentaa sen sessionStorageen
@@ -53,7 +61,6 @@ app.controller('MainController', ['$scope', 'ChatWindow', '$timeout', '$log', '$
             $scope.question = $scope.data.question;
             $scope.options = $scope.data.answers;
         }
-
         $log.info($scope.data);
 
         var questionSpell = function(x){
@@ -66,25 +73,33 @@ app.controller('MainController', ['$scope', 'ChatWindow', '$timeout', '$log', '$
             $interval(spell, 30, $scope.que.length);
         }
 
-
         $scope.answerFunction = function(qid, aid){
-
             var answer = JSON.stringify({session_id:JSON.parse(sessionStorage.getItem("sessionId")).id, question_id:$scope.id, answer_id:aid});
-
             $log.info(answer);
-
             $http.post('/save-answer/', answer).success(function (data) {
                 $log.info(data);
             }).error(function (err) {
                 $log.info("Tapahtui virhe");
             })
 
-
                 $scope.hide = true;
                 $timeout(function () {
                     $http.get('/question/' + qid).success(function (data) {
                         $scope.id = data.id;
+                        $scope.alku = data.question.search("http");
+                        if($scope.alku > -1){
+                        $log.info("alku: " + $scope.alku);
+                            if(data.question.search(".fi") == -1){
+                                $scope.loppu = data.question.search(".com") + 4;
+                            } else if(data.question.search(".com") == -1){
+                                $scope.loppu = data.question.search(".fi") + 3;
+                            }
+                            $scope.testi = data.question;
+                            $scope.question = $scope.testi.substring(0, $scope.alku) + "<a href='" + $scope.testi.substring($scope.alku, $scope.loppu) + "'>" + $scope.testi.substring($scope.alku, $scope.loppu) + "</a>" + $scope.testi.substring($scope.loppu);
+                        questionSpell($scope.question);
+                        } else {
                         questionSpell(data.question);
+                        }
                         $scope.options = data.answers;
                         $log.info(data);
                     }).error(function (err) {
@@ -104,6 +119,5 @@ app.controller('MainController', ['$scope', 'ChatWindow', '$timeout', '$log', '$
                         $log.info("Tapahtui virhe");
                    })
         };
-
     });
 }]);
